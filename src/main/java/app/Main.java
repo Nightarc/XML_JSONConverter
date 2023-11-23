ce78ed1f210cd6ca96d627be4c908d161199189d
Nightarc <yurik.1594@gmail.com>
2023-11-24 02:14:58 +0300

    Implented console interface and maven assembly plugin for jar packaging.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -4,32 +4,51 @@ import java.io.*;
 
 import app.converter.JSON;
 import app.converter.XML;
-import lombok.val;
 
 public class Main {
+    enum convertType
+    {
+        XML_TO_JSON,
+        JSON_TO_XML,
+        INVALID
+    }
     public static void main(String[] args) {
-        val xmlInput = "src/test/resources/SpellXML.xml";
-        val jsonInput = "src/test/resources/outputJSON.json";
-        val jsonOutput = "src/test/resources/outputJSON.json";
-        val xmlOutput = "src/test/resources/outputTestXML.xml";
-        File xmlInputFile = new File(xmlInput);
-        File jsonInputFile = new File(jsonInput);
-        File xmlOutputFile = new File(xmlOutput);
-        File jsonOutputFile = new File(jsonOutput);
+        XML XMLHelper = new XML();
+        JSON JSONHelper = new JSON();
 
-        XML xmlConverter = new XML();
-        JSON jsonConverter = new JSON();
-        try {
-            jsonConverter.write(xmlConverter.read(xmlInputFile), jsonOutputFile);
-            xmlConverter.write(jsonConverter.read(jsonInputFile), xmlOutputFile);
-        }
-        catch(Exception e)
+        if(args.length == 2)
         {
-            System.out.println(e.getMessage());
+            try {
+                File inputFile = new File(args[0]);
+                File outputFile = new File(args[1]);
+                switch(defineConversionType(args))
+                {
+                    case XML_TO_JSON -> JSONHelper.write(XMLHelper.read(inputFile), outputFile);
+                    case JSON_TO_XML -> XMLHelper.write(JSONHelper.read(inputFile), outputFile);
+                    default -> System.out.println("Такой тип преобразования не поддерживается.");
+                }
+            }
+            catch(Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
+        else
+        {
+            System.out.println("Use: .jar <input-file> <output-file>");
         }
     }
 
+    public static convertType defineConversionType(String[] args)
+    {
+        if (args[0].endsWith(".xml") && args[1].endsWith(".json")) {
+            return convertType.XML_TO_JSON;
 
+        }
+        if (args[0].endsWith(".json") && args[1].endsWith(".xml")) {
+            return convertType.JSON_TO_XML;
+        }
 
-
+        return convertType.INVALID;
+    }
 }
